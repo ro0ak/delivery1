fa999cf57bc142e3e056f63919820e9ea5b400dd
ro0ak <royal201391h@gmail.com>
2026-07-29 01:49:48 +0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,29 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router";
+import { Toaster } from "sonner";
+import App from "./app/App";
+import { AuthProvider } from "./app/contexts/AuthContext";
+import "./styles/index.css";
 
-  import { createRoot } from "react-dom/client";
-  import App from "./app/App.tsx";
-  import "./styles/index.css";
+const rootElement = document.getElementById("root");
 
-  createRoot(document.getElementById("root")!).render(<App />);
-  
+if (!rootElement) {
+  throw new Error("Root element was not found.");
+}
+
+createRoot(rootElement).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <AuthProvider>
+        <App />
+        <Toaster
+          position="top-center"
+          richColors
+          closeButton
+          dir="rtl"
+        />
+      </AuthProvider>
+    </BrowserRouter>
+  </React.StrictMode>,
+);
